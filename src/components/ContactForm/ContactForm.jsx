5c22ddd55bedbb941e02d308892a5ce28e8bbc42
Taras Kovalyk <tarik2454@gmail.com>
2023-08-04 00:42:38 +0300

    Adds styles and PropTypes

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { StyledForm, StyledInput, StyledButton } from './ContactForm.styled';
 
 export class ContactForm extends Component {
   state = {
-    contacts: [],
     name: '',
+    number: '',
+  };
+
+  handleChange = event => {
+    const { name, value, number } = event.target;
+
+    this.setState({ [name]: value, [number]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { name, number } = this.state;
+
+    this.props.onAddContact({ name, number });
+    this.setState({ name: '', number: '' });
   };
 
   render() {
     return (
-      <form>
-        <input
+      <StyledForm onSubmit={this.handleSubmit}>
+        <StyledInput
           type="text"
           name="name"
+          placeholder="Name"
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
+          onChange={this.handleChange}
+          value={this.state.name}
+        />
+        <StyledInput
+          type="tel"
+          name="number"
+          placeholder="Phone number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          onChange={this.handleChange}
+          value={this.state.number}
         />
 
-        <button type="submit">Add contact</button>
-      </form>
+        <StyledButton>Add contact</StyledButton>
+      </StyledForm>
     );
   }
 }
+
+ContactForm.propTypes = { onAddContact: PropTypes.func.isRequired };
